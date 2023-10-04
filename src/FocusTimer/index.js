5dfc60f5ce7daac80649ce3cbb2b2b76ce73377d
gabriel <gabrielscotio@gmail.com>
2023-10-04 17:24:23 -0300

    controls added

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,3 +1,7 @@
-export function start (minutes, seconds) {
+import state from "./state.js"
+import * as events from "./events.js"
 
+export function start (minutes, seconds) {
+    state.minutes = minutes
+    state.seconds = seconds
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 import "./toggle.js"
 import * as FocusTimer from "./FocusTimer/index.js"
+
+FocusTimer.start(0,6)
